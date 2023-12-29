0e0fe07f58216a574e0ea0367a63e14b40d6532a
VarunKS <ksvarun001@gmail.com>
2023-12-29 12:28:40 +0530

    05.animations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const canvas = document.querySelector('#webgl');
 const sizes = { width: 800, height: 600 };
@@ -17,7 +18,7 @@ const scene = new THREE.Scene();
 // scene.add(mesh);
 
 //group
-const group = new THREE.Group();
+// const group = new THREE.Group();
 const mesh1 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: '#fff' })
@@ -26,15 +27,16 @@ const mesh2 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: '#fffaaa' })
 );
-mesh1.position.set(-0.75, 0, 1);
+mesh1.position.set(0, 0, 0);
 mesh2.position.set(0.75, 0, 1);
-group.add(mesh1);
-group.add(mesh2);
-scene.add(group);
+// group.add(mesh1);
+// group.add(mesh2);
+scene.add(mesh1);
+// scene.add(mesh2);
 
 //AxesHelper adds three co-ordinate lines to the origin of the scene
-const axisHelper = new THREE.AxesHelper();
-scene.add(axisHelper);
+// const axisHelper = new THREE.AxesHelper();
+// scene.add(axisHelper);
 
 //scale
 // mesh.scale.set(2, 1, 1);
@@ -54,7 +56,7 @@ camera.position.z = 4;
 scene.add(camera);
 
 //lookAt will point the camera to point to different position of the scene
-// camera.lookAt(new THREE.Vector3(3, 0, 0));
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 //distance between origin and the object
 // console.log(mesh.position.length());
@@ -68,4 +70,27 @@ scene.add(camera);
 //renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+console.log(gsap);
+gsap.to(mesh1.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh1.position, { duration: 1, delay: 2, x: 0 });
+
+//since the frame rate is different for different computers we need to normalise it
+// let time = Date.now();
+// const clock = new THREE.Clock();
+//animation
+(function tick() {
+  //   const currentTime = Date.now();
+  //   const deltaTime = currentTime - time;
+  //   time = currentTime;
+  //   mesh1.rotation.y += 0.002 * deltaTime;
+  //   const elapsedTime = clock.getElapsedTime();
+  //to get one full rotation
+  //   mesh1.rotation.y = elapsedTime * Math.PI * 2;
+  //   mesh1.rotation.y = elapsedTime;
+  //   mesh1.position.x = Math.sin(elapsedTime);
+  //   mesh1.position.y = Math.cos(elapsedTime);
+  //when camera is rotating and we use lookat it does not cause any difference when there is only on object
+  //   camera.lookAt(mesh1.position);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+})();
